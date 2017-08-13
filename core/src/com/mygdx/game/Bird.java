f8634a21ff3a0ff37079e019be9ebe388dcbdd20
infuntis <irchin.kiedis@gmail.com>
2017-08-13 16:11:39 +0300

    initial

---
+++ core/src/com/mygdx/game/Bird.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by infuntis on 13/08/17.
+ */
+public class Bird {
+    Texture img;
+    Vector2 position;
+    float vy;
+    float gravity;
+
+
+    public Bird(){
+        img = new Texture("bird1.png");
+        position = new Vector2(100,380);
+        vy = 0;
+        gravity = - 0.7f;
+    }
+
+    public void render(SpriteBatch batch){
+        batch.draw(img, position.x, position.y);
+    }
+
+    public void update(){
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            vy = 10;
+        }
+        vy += gravity;
+        position.y += vy;
+    }
+}
---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,45 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Background bg;
+	Bird bird;
+
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		bg = new Background();
+		bird = new Bird();
+
+	}
+
+	@Override
+	public void render () {
+		update();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		bg.render(batch);
+		bird.render(batch);
+		batch.end();
+	}
+
+	public void update(){
+		bg.update();
+		bird.update();
+
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
